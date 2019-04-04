489f2fb3eb45240778094939a877a2686744acea
Cory Pavitt <corypavitt@hotmail.com>
2019-04-03 21:55:02 -0400

    update cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'  # change this before launch
+    origins 'https://kids-star-chart.herokuapp.com/'  # change this before launch
 
     resource '*',
       headers: :any,
