bbba42848f00b7257d89c259c4728e30b5d0fb06
Cory Pavitt <corypavitt@hotmail.com>
2019-04-03 22:25:29 -0400

    update cors again4

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -12,8 +12,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-      expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
-                   max_age: 0
+      
 
   end
 end
